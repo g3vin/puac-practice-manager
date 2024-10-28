2144bc56f984fa5353dc54d4c82d790813dd0d0f
g3vin <gbowden@purdue.edu>
2024-10-28 01:13:09 -0400

    updated main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-// src/main.jsx
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import './firebase-config';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-);
+)
