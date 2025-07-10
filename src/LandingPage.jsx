74235d3aaf1eeac78821116ddce6e53557587829
g3vin <gbowden@purdue.edu>
2025-07-09 23:48:06 -0400

    More nav and visual changes

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,14 +1,18 @@
 import { useNavigate } from 'react-router-dom';
 import "./LandingPage.css";
+import HomeNavbar from './HomeNavbar';
 
 function LandingPage() {
   const navigate = useNavigate();
 
   return (
-    <div className='landing-page'>
-      <h1>Purdue Archery</h1>
-      <button onClick={() => navigate('/login')}>Sign in</button>
-    </div>
+    <>
+        <HomeNavbar />
+        <div className='landing-page'>
+        <h1>Purdue Archery</h1>
+        <button onClick={() => navigate('/login')}>Sign in</button>
+        </div>
+    </>
   );
 }
 
